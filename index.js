064052533a91c1219620ad7e621664c43eebe71e
unknown <yunussahin9358@gmail.com>
2021-06-24 13:51:40 +0300

    git,linkedin atanmış hali

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var btnfullName = document.getElementById("fullName");
 var fName="Yunus";
 var lName="Şahin"
 var age = "28";
+var btnfullName = document.getElementById("fullName");
 btnfullName.onclick=function(){
     document.getElementById("nameSurname").innerHTML =fName + " " + lName;
     document.getElementById("age").innerHTML =age;
 }
+document.getElementById("linkedinUrl").setAttribute("href","https://www.linkedin.com/in/yunusssahin/");
+document.getElementById("githubUrl").setAttribute("href" , "https://github.com/yunusssahin");
